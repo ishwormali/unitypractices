d8b1b48a4a1bcc08fea8582b37edffcf8727bff7
Ish <ishanbuddy@gmail.com>
2017-12-28 15:19:45 +1100

    added enemy

---
+++ TowerDefence/Assets/Scripts/WayPoints.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WayPoints : MonoBehaviour {
+
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+}
