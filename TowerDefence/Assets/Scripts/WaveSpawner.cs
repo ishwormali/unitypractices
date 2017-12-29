87a9257609148e1d86c322c4afadc98d57b08105
Ish <ishanbuddy@gmail.com>
2017-12-29 12:29:00 +1100

    enemy spawner

---
+++ TowerDefence/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    public Transform spawnPoint;
+    private int waveIndex = 0;
+
+    public Text waveCountdownText;
+
+	// Update is called once per frame
+	void Update () {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+	}
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab,spawnPoint.position,spawnPoint.rotation);
+    }
+}
